6c036df7d8a0ecc53d113ffd193c049f489d54ef
SkyProAuto-HW <e730spam@gmail.com>
2023-02-04 17:46:28 +0400

    Домашняя работа1 попытка1

---
+++ lesson_1_task_7.py
@@ -0,0 +1,32 @@
+def print_console_1():
+    print("1")
+def print_console_2():
+    print("2")
+def print_console_3():
+    print("3")
+def print_console_4():
+    print("4")
+def print_console_5():
+    print("5")
+def print_console_6():
+    print("6")
+def print_console_7():
+    print("7")
+def print_console_8():
+    print("8")
+def print_console_9():
+    print("9")
+def print_console_0():
+    print("0")
+
+print_console_8()
+print_console_8()
+print_console_0()
+print_console_0()
+print_console_5()
+print_console_5()
+print_console_5()
+print_console_3()
+print_console_5()
+print_console_3()
+print_console_5()
