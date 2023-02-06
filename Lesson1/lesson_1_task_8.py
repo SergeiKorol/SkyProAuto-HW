71cd390461920dd405d6543e58e5ce541ec750e3
SkyProAuto-HW <e730spam@gmail.com>
2023-02-06 14:01:02 +0400

    домашка1 попытка2

--- Lesson1/lesson_1_task_8.py
+++ Lesson1/lesson_1_task_8.py
@@ -1,4 +1,8 @@
-def print_num(num):
-    print(num)
+
+def print_num (a1, b2, c3, d4, e5, f6, g7, h8, i9, j0):
+    print(h8, h8, j0, j0, e5, e5, e5, c3, e5, c3, e5)
+
+print_num(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
     
+
 
