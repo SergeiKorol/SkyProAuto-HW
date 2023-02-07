571a6f6b47c48aebc19b1e6e26d0a8ffd2d48c87
SkyProAuto-HW <e730spam@gmail.com>
2023-02-08 00:42:12 +0400

    домашка2 заход1

--- Lesson1/lesson_1_task_5.py
+++ Lesson1/lesson_1_task_5.py
@@ -1,5 +1,5 @@
 my_age = input("Введите вашш возвраст")
 print("Ваш возвраст: " + my_age)
-int(my_age)
+int([my_age])
 my_age = (my_age + 1)
 print("Ваш возвраст: " + my_age)
---
+++ Lesson2/lesson_2_task_2.py
@@ -0,0 +1,8 @@
+def is_year_leap(a):
+    if (a % 4) > 0:
+        print('True')
+    else: print('False')
+
+a = input('Введите год=')
+int([a])
+is_year_leap(a)
